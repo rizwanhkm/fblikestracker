17c3c4b901a547c21aa2a51ec6c54c2d3941ada8
Rizwan <rizwan.hkm@gmail.com>
2014-09-25 17:12:23 +0530

    Delete login-redirect.js

--- login-redirect.js
+++
@@ -1,4 +0,0 @@
-function redirect(){
-
-        window.location="https://www.facebook.com/dialog/oauth?client_id=694159134003863&redirect_uri=http://delta.nitt.edu/~schezwan/fblikes/login-process.php&scope=public_profile,user_friends,email";
-            }
